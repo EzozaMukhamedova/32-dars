64576c10bec590bba2e46fc150b67aea0f5df80f
EzozaMukhamedova <ezoza.muxamedova@gmail.com>
2024-11-23 05:50:35 +0900

    32-dars

---
+++ js/index.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+  theme: {
+    container: {
+      center: true,
+      widht: {
+        tablet: "682px",
+        desktop: "1170px",
+      },
+      padding: {
+        DEFOLT: "30px",
+        tablet: "0",
+      },
+    },
+    screens: {
+      tablet: "780px",
+      desktop: "1280px",
+    },
+    extends: {
+      colors: {},
+    },
+  },
+};
